c1ca8e7f0af6c9be3a51e9dc7903af5e6ac5dcbc
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-06-29 18:12:34 -0400

    not sure but I'm on the right track

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//get reqest for ya basic endpoint
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "Gasaplemop1!",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+//a get reqest for ya basic endpoint
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World",
